831744a54d8824e10afc287c2f97d59f1a033d61
Oka Ayuko <oka.ayuko0210@gmail.com>
2022-06-30 20:14:48 +0700

    OK

--- user/js/map.js
+++ user/js/map.js
@@ -5,6 +5,7 @@ var infowindow = null;
 var ddisplay = null;
 var dservice = null;
 var radius_circle = null;
+var buttonDirection;
 var markerArray = [];
 var parkingLocations = [{
         "name": "Bãi giữ xe",
@@ -60,12 +61,49 @@ var parkingLocations = [{
         "lat": 10.829448363525294,
         "long": 106.57226481009462,
     },
+    {
+        "name": "Bãi xe 244",
+        "address": "336 Liên Khu 4-5, Bình Hưng Hoà B, Bình Tân, Thành phố Hồ Chí Minh, Việt Nam",
+        "lat": 10.803836157761179,
+        "long": 106.5833460898702,
+    },
+    {
+        "name": "Bãi giữ xe ô tô Tháng",
+        "address": "47/8 Bình Thành, Bình Hưng Hoà B, Bình Tân, Thành phố Hồ Chí Minh, Việt Nam",
+        "lat": 10.801606628483857,
+        "long": 106.58653293093546,
+    },
+    {
+        "name": "Bãi xe Bá Sơn",
+        "address": "152 Đ. Nguyễn Thị Tú, Bình Hưng Hoà B, Bình Tân, Thành phố Hồ Chí Minh, Việt Nam",
+        "lat": 10.815494097294602,
+        "long": 106.59142729657972,
+    },
+    {
+        "name": "Bãi xe 390",
+        "address": "390 QL1A, Bình Hưng Hoà B, Bình Tân, Thành phố Hồ Chí Minh, Việt Nam",
+        "lat": 10.806340194668364,
+        "long": 106.59876903376622,
+    },
+    {
+        "name": "Bãi đậu xe khu công nghiệp Tân Bình",
+        "address": "Đường CN6, Sơn Kỳ, Tân Phú, Thành phố Hồ Chí Minh, Việt Nam",
+        "lat": 10.808647358759579,
+        "long": 106.61051074577816,
+    },
+    {
+        "name": "Bãi đậu xe nhà hàng Đông Phương 4",
+        "address": "309 Đông Hưng Thuận 29, Đông Hưng Thuận, Quận 12, Thành phố Hồ Chí Minh, Việt Nam",
+        "lat": 10.835440504398681,
+        "long": 106.6285604770306,
+    },
 ]
-
 
 function showPlace(latpos, longpos) {
     var latpos = latpos.toFixed(6);
     var longpos = longpos.toFixed(7);
+    let listItem = document.querySelector(".list-item");
+    let exerciseItems = "";
     //console.log(latpos, longpos);
     for (var i = 0; i < parkingLocations.length; i++) {
         var latParking = parkingLocations[i]['lat'].toFixed(6);
@@ -83,18 +121,27 @@ function showPlace(latpos, longpos) {
         var difflat = (dLat2 - dLat1);
         var difflon = (longParking - longpos) * (Math.PI / 180);
         var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat / 2) * Math.sin(difflat / 2) + Math.cos(dLat1) * Math.cos(dLat2) * Math.sin(difflon / 2) * Math.sin(difflon / 2)));
+        //console.log(d);
         // show multiple marker
-        console.log(d);
         if (d < radius) {
             var markerParking, i;
+            var pos = new google.maps.LatLng(parkingLocations[i]['lat'], parkingLocations[i]['long']);
+            //console.log(pos);
+            // add list
+            exerciseItems += "<div class='card p-2'>" + "<h6 class='' style='font-style:bold;'>" + parkingLocations[i].name + "</h6>" +
+                "<p style='font-size:14px;'>" + parkingLocations[i].address + "</p>" + "<div style='font-size:12px;'><i class='fas fa-road'></i> " + d.toFixed(2) +
+                " km " + "</div>" + "</div><br/>";
+            // icon
             var iconParking = {
                 url: "img/marker.png",
+                size: new google.maps.Size(71, 71),
                 scaledSize: new google.maps.Size(45, 45),
                 origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(0, 0)
-            }
+                anchor: new google.maps.Point(17, 34),
+            };
+            // create marker
             markerParking = new google.maps.Marker({
-                position: new google.maps.LatLng(parkingLocations[i]['lat'], parkingLocations[i]['long']),
+                position: pos,
                 map: map,
                 icon: iconParking,
                 content: '<div id="content">' +
@@ -111,6 +158,8 @@ function showPlace(latpos, longpos) {
             })(markerParking, i));
         }
     }
+    // display html list
+    listItem.innerHTML = exerciseItems;
 }
 
 function showDirection(data) {
@@ -133,14 +182,18 @@ function showDirection(data) {
         if (status == "OK") {
             $("button").on("click", function() {
                 //console.log(result);
-                ddisplay.setDirections(result);
-                document.getElementById("distance").setAttribute('value', 'Khoảng Cách: ' + (result.routes[0].legs[0].distance.value / 1000).toFixed(3) + ' km');
-                document.getElementById("duration").setAttribute('value', 'Thời Gian: ' + result.routes[0].legs[0].duration.text);
+                buttonDirection = $(this).attr('id');
+                if (buttonDirection == "direction") {
+                    ddisplay.setDirections(result);
+                    // document.getElementById("distance").setAttribute('value', 'Khoảng Cách: ' + (result.routes[0].legs[0].distance.value / 1000).toFixed(3) + ' km');
+                    // document.getElementById("duration").setAttribute('value', 'Thời Gian: ' + result.routes[0].legs[0].duration.text);
+                }
             })
         }
     });
 }
 
+// show map
 function showMap() {
     inforwindow = new google.maps.InfoWindow();
     window.navigator.geolocation.getCurrentPosition(function(pos) {
@@ -149,7 +202,8 @@ function showMap() {
         map = new google.maps.Map(document.getElementById("map"), {
             center: { lat: lat, lng: long },
             zoom: 14,
-            fullscreenControl: false
+            fullscreenControl: false,
+            streetViewControl: false,
         });
         var markerLocation = new google.maps.Marker({
             position: { lat: lat, lng: long },
@@ -162,5 +216,4 @@ function showMap() {
         }));
         showPlace(this.lat, this.long);
     })
-
 }
