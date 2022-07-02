59cf7221f604e062cbccfa82e456cf2168357bfc
Oka Ayuko <oka.ayuko0210@gmail.com>
2022-07-02 22:02:24 +0700

    OK

---
+++ user/js/fix.js
@@ -0,0 +1,5 @@
+function redisable() {
+    document.getElementById('save').style.display = "inline-block";
+    document.getElementById('cancel').style.display = " inline-block";
+    document.getElementById('edit').style.display = "none";
+}
