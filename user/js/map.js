9a108f0cf6084e9e9279f6b854b77aad29f8fd14
Oka Ayuko <oka.ayuko0210@gmail.com>
2022-07-04 20:12:35 +0700

    OK

--- user/js/map.js
+++ user/js/map.js
@@ -8,91 +8,85 @@ var radius_circle = null;
 var buttonDirection;
 var markerArray = [];
 var parkingLocations = [{
-        "name": "Bãi giữ xe",
-        "address": "Cách Mạng Tháng Tám, Phường 15, Quận 10, Thành phố Hồ Chí Minh, Việt Name",
+        "name": "Lê Thị Riêng",
+        "address": "Cách Mạng Tháng Tám, Phường 15, Quận 10, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.786140368621982,
         "long": 106.66553523925666,
     },
     {
-        "name": "Bãi giữ xe",
+        "name": "Trường Sơn",
         "address": "Trường Sơn, Phường 15, Quận 10, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.783623768095028,
         "long": 106.66602110517302,
     },
     {
-        "name": "Bãi giữ xe ôtô",
+        "name": "550 CMT8",
         "address": "550 Đ. Cách Mạng Tháng 8, Phường 11, Quận 3, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.785919366395701,
         "long": 106.66687897069089,
     },
     {
-        "name": "Bãi giữ xe bệnh viện Thống Nhất",
+        "name": "Bệnh Viện Thống Nhất",
         "address": "669 Đ. Cách Mạng Tháng 8, Phường 6, Tân Bình, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.792474542422955,
         "long": 106.65389890720036,
     },
     {
-        "name": "Bãi giữ xe 24h",
+        "name": "332 Cao Thắng",
         "address": "332 Đ. Cao Thắng, Phường 12, Quận 10, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.775856472760822,
         "long": 106.66872907259513,
     },
     {
-        "name": "Bãi đậu xe Cao Thắng",
+        "name": "117 Cao Thắng",
         "address": "177 Đ. Cao Thắng, Phường 12, Quận 10, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.77460735384704,
         "long": 106.67606615637462,
     },
     {
-        "name": "Bãi đậu xe Cao Thắng",
-        "address": "177 Đ. Cao Thắng, Phường 12, Quận 10, Thành phố Hồ Chí Minh, Việt Nam",
-        "lat": 10.774602038976802,
-        "long": 106.67606315432558,
-    },
-    {
-        "name": "Bãi giữ xe Thái",
+        "name": "Tổ 5 Bình Chánh",
         "address": "Đường Trục, Tổ 5, Bình Chánh, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.804105422754187,
         "long": 106.56876503892447,
     },
     {
-        "name": "Bãi giữ xe ô tô 24/24",
+        "name": "2A4 Quách Điêu",
         "address": "2A4 Quách Điêu, Vĩnh Lộc A, Bình Chánh, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.829448363525294,
         "long": 106.57226481009462,
     },
     {
-        "name": "Bãi xe 244",
+        "name": "244B BHHB",
         "address": "336 Liên Khu 4-5, Bình Hưng Hoà B, Bình Tân, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.803836157761179,
         "long": 106.5833460898702,
     },
     {
-        "name": "Bãi giữ xe ô tô Tháng",
+        "name": "47/8 Bình Thành",
         "address": "47/8 Bình Thành, Bình Hưng Hoà B, Bình Tân, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.801606628483857,
         "long": 106.58653293093546,
     },
     {
-        "name": "Bãi xe Bá Sơn",
+        "name": "152 Bá Sơn",
         "address": "152 Đ. Nguyễn Thị Tú, Bình Hưng Hoà B, Bình Tân, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.815494097294602,
         "long": 106.59142729657972,
     },
     {
-        "name": "Bãi xe 390",
+        "name": "390 BHHB",
         "address": "390 QL1A, Bình Hưng Hoà B, Bình Tân, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.806340194668364,
         "long": 106.59876903376622,
     },
     {
-        "name": "Bãi đậu xe khu công nghiệp Tân Bình",
+        "name": "Khu Công Nghiệp Tân Bình",
         "address": "Đường CN6, Sơn Kỳ, Tân Phú, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.808647358759579,
         "long": 106.61051074577816,
     },
     {
-        "name": "Bãi đậu xe nhà hàng Đông Phương 4",
+        "name": "Nhà Hàng Đông Phương 4",
         "address": "309 Đông Hưng Thuận 29, Đông Hưng Thuận, Quận 12, Thành phố Hồ Chí Minh, Việt Nam",
         "lat": 10.835440504398681,
         "long": 106.6285604770306,
@@ -128,9 +122,9 @@ function showPlace(latpos, longpos) {
         var radius = 10.000;
         // show multiple marker
         if (cal < radius) {
-            exerciseItems += "<li><div class='shop-item'>" + "<a>" + parkingLocations[i].name + "</a>" +
+            exerciseItems += "<li'><div class='shop-item' id='shop-item'>" + "<a id='shop-item-name'>" + parkingLocations[i].name + "</a>" +
                 "<p>" + parkingLocations[i].address + "</p>" + "<p>" + "<i class='fa fa-road'></i> " + cal.toFixed(3) + " km" +
-                "<button type='button' class='direction' id=''><i class='fa fa-directions'></i> Chỉ Đường</button>" + "</p>" + "</div>" + "</li>";
+                "<button type='button' class='booking-list' id=''><i class='fas fa-parking'></i> Booking</button>" + "</p>" + "</div>" + "</li>";
             //console.log(req.des);
             var markerParking, i;
             var pos = new google.maps.LatLng(parkingLocations[i]['lat'], parkingLocations[i]['long']);
@@ -156,12 +150,36 @@ function showPlace(latpos, longpos) {
                 return function() {
                     inforwindow.setContent(this.content);
                     inforwindow.open(map, markerParking);
+                    map.panTo(this.position);
                     showDirection(this.position);
-                    //console.log(this.position);
+                    console.log(this.position);
                 }
             })(markerParking, i));
         }
     }
+    // booking and show marker on list
+    $(document).ready(function() {
+        $('#shop-item #shop-item-name').click(function() {
+            var idParking = this.textContent;
+            for (var i in parkingLocations) {
+                if (idParking == parkingLocations[i]['name']) {
+                    var posI = new google.maps.LatLng(parkingLocations[i]['lat'], parkingLocations[i]['long']);
+                    markerParking = new google.maps.Marker({
+                        position: posI,
+                        map: map,
+                        icon: iconParking,
+                    });
+                    inforwindow.setContent('<div id="content-parking">' +
+                        '<strong style="font-weight:600; font-size:14px;">' + parkingLocations[i]['name'] + '</strong>' +
+                        '<br/>' + parkingLocations[i]['address'] + '<div class="p-1"></div>' +
+                        '<button type="button" class="direction" id="direction">Chỉ Đường <i class="fas fa-directions"></i></button>', );
+                    inforwindow.open(map, markerParking);
+                    map.panTo(posI);
+                    showDirection(posI);
+                }
+            }
+        });
+    });
     // display html list
     listItem.innerHTML = exerciseItems;
 }
@@ -195,7 +213,6 @@ function showDirection(data) {
         }
     });
 }
-
 
 function getPosition() {
     if (!navigator.geolocation) {
